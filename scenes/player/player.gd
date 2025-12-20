1ef30ca347769e5f688e172dd2c0832ca9faebe2
vivithequeen <vivithequeen1@gmail.com>
2025-12-19 21:44:34 -0500

    animation trenchbroom and other stuff

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,42 +1,59 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 3.5
 const JUMP_VELOCITY = 4.5
 const LOOKSENSE := (0.0025 /2.0) * 4.0
 
+
+@onready var anim_tree = $Node3D/AnimationTree
 var paused := false
 
-var idletowalk : float = 0.0;
+var walk_forwards = 0.0;
+var walk_backwards = 0.0;
+var walk_right = 0.0;
+var walk_left = 0.0;
+var run = 0.0
 func _ready() -> void:
 	update_mouse_mode()
 
 func _physics_process(delta: float) -> void:
+	var is_sprint = Input.is_action_pressed("sprint")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * (1.4 if is_sprint else 1.0)
+		velocity.z = direction.z * SPEED * (1.4 if is_sprint else 1.0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	if (velocity * Vector3(1,0,1) != Vector3.ZERO):
-		var tween = get_parent().create_tween()
-		tween.tween_property(self, "idletowalk", 1.0, delta);
 
-	$Node3D/AnimationTree.set("parameters/Blend2/blend_amount", idletowalk)
-	print($Node3D/AnimationTree.get("parameters/idletoWalk/blend_amount")
-)
+	var tween = get_parent().create_tween()
+	tween.tween_property(self, "walk_forwards", (1.0 if   input_dir.y > 0 else 0.0), delta*10.0);
+	tween.parallel()
+	tween.tween_property(self, "walk_backwards", (1.0 if input_dir.y < 0 else 0.0), delta*10.0);
+	tween.parallel()
+	tween.tween_property(self, "walk_right", (1.0 if     input_dir.x > 0 else 0.0), delta*10.0);
+	tween.parallel()
+	tween.tween_property(self, "walk_left", (1.0 if     input_dir.x < 0 else 0.0), delta*10.0);
+	tween.parallel()
+	
+	tween.tween_property(self, "run", 1.0 if (is_sprint and input_dir) else 0.0, delta*10.0);
+
+	anim_tree["parameters/walk_forwards/blend_amount"] =  walk_forwards
+	anim_tree["parameters/walk_backwards/blend_amount"] = walk_backwards 
+	anim_tree["parameters/walk_right/blend_amount"] =  walk_right
+	anim_tree["parameters/walk_left/blend_amount"] =  walk_left
+	anim_tree["parameters/run/blend_amount"] = run
+	
+
 
 
 	move_and_slide()
